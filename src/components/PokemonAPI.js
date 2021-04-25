c622c0ea8da8ff2c4823da2fe3d3d0be5c039767
Reggie Amedee <jeantechnical1992@gmail.com>
2021-04-25 14:25:41 -0400

    working_with_pokemon_api

---
+++ src/components/PokemonAPI.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const PokemonAPI = (props) => {
+
+const[pokemon, setPokemon] = useState([]);
+
+useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=807")
+    .then(response => response.json())
+    .then(response => setPokemon(response.results))
+},[])
+
+return(
+    <div>
+        {pokemon.map((pokemon, index) => {
+            return (<div key={index}>{pokemon.name}</div>)
+        })}
+    </div>
+
+)
+
+}
+export default PokemonAPI;
+
